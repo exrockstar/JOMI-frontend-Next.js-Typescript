37bb7d1d3acb2f4d80ab983ca39af9376a35942d
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-07-16 07:37:03 +0800

    Add _name property

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -162,6 +162,7 @@ export const getStaticProps: GetStaticProps<any, IParams> = async ({
     })
     return {
       props: {
+        _name: 'generic',
         page: {
           ...data?.pageBySlug,
           content: content
